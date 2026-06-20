311cb2bc091e67f9efb850943d68606c22ba96bf
arthurdelfino <arthurdelfino@users.noreply.github.com>
2026-06-20 20:46:00 -0300

    Mudanca Titulo

--- VIEW/listas.php
+++ VIEW/listas.php
@@ -8,10 +8,10 @@ exigirLogin();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Gerenciar Listas</title>
+  <title>Gerenciar CRUDS</title>
 </head>
 <body>
-  <h1>Gerenciar Listas</h1>
+  <h1>Gerenciar CRUDS</h1>
 
   <ul>
     <li><a href="autor/lstAutor.php">Autores</a></li>
